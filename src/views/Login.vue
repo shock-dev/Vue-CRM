b094b656c2e56206d8b2085d4b12ae8add4431ce
Andrey <shock.work@mail.ru>
2020-11-30 16:51:39 +0300

    validation login form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -6,19 +6,43 @@
         <input
             id="email"
             type="text"
-            class="validate"
+            v-model.trim="email"
+            :class="{invalid: ($v.email.$dirty && !$v.email.required) || ($v.email.$dirty && !$v.email.email)}"
         >
         <label for="email">Email</label>
-        <small class="helper-text invalid">Email</small>
+        <small
+            v-if="$v.email.$dirty && !$v.email.required"
+            class="helper-text invalid"
+        >
+          Поле Email должно быть заполнено
+        </small>
+        <small
+            v-else-if="$v.email.$dirty && !$v.email.email"
+            class="helper-text invalid"
+        >
+          Введите корректный email
+        </small>
       </div>
       <div class="input-field">
         <input
             id="password"
             type="password"
-            class="validate"
+            v-model.trim="password"
+            :class="{invalid: ($v.password.$dirty && !$v.password.required) || ($v.password.$dirty && !$v.password.minLength)}"
         >
         <label for="password">Пароль</label>
-        <small class="helper-text invalid">Password</small>
+        <small
+            v-if="$v.password.$dirty && !$v.password.required"
+            class="helper-text invalid"
+        >
+          Введите пароль
+        </small>
+        <small
+            v-else-if="$v.password.$dirty && !$v.password.minLength"
+            class="helper-text invalid"
+        >
+          Пароль должен содержать минимум 6 символов. Сейчас {{ password.length }}
+        </small>
       </div>
     </div>
     <div class="card-action">
@@ -41,13 +65,41 @@
 </template>
 
 <script>
+import {email, required, minLength} from 'vuelidate/lib/validators'
+
 export default {
   name: "Login",
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
   validations: {
-
+    email: {
+      email,
+      required
+    },
+    password: {
+      required,
+      minLength: minLength(6)
+    }
   },
   methods: {
     submitHandler() {
+      if (this.$v.$invalid) {
+        this.$v.$touch()
+
+        return
+      }
+
+      const formDate = {
+        email: this.email,
+        password: this.password
+      }
+
+      console.log(formDate)
+
       this.$router.push('/')
     }
   }
