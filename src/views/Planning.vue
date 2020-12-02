18ccf6035797bf021065f43d3a240646d7bec212
Andrey <shock.work@mail.ru>
2020-12-03 00:00:41 +0300

    planning calculation

--- src/views/Planning.vue
+++ src/views/Planning.vue
@@ -2,19 +2,27 @@
   <div>
     <div class="page-title">
       <h3>Планирование</h3>
-      <h4>12 212</h4>
+      <h4>{{ info.bill | currency }}</h4>
     </div>
 
-    <section>
-      <div>
+    <loader v-if="loading"/>
+
+    <p v-else-if="!categories.length" class="center">Категорий еще нет. <router-link :to="{name: 'categories'}">Создать категорию</router-link></p>
+
+    <section v-else>
+      <div
+        v-for="cat in categories"
+        :key="cat.id"
+      >
         <p>
-          <strong>Девушка:</strong>
-          12 122 из 14 0000
+          <strong>{{ cat.title }}:</strong>
+          {{ cat.spent | currency }} из {{ cat.limit | currency }}
         </p>
         <div class="progress" >
           <div
-              class="determinate green"
-              style="width:40%"
+              class="determinate"
+              :class="cat.progressColor"
+              :style="{width: cat.progressPercent + '%'}"
           ></div>
         </div>
       </div>
@@ -23,8 +31,49 @@
 </template>
 
 <script>
+import {mapGetters} from 'vuex'
+
 export default {
-name: "Planning"
+  name: "Planning",
+  data: () => ({
+    loading: true,
+    categories: []
+  }),
+  computed: {
+    ...mapGetters([
+        'info'
+    ])
+  },
+  async mounted() {
+    const categories = await this.$store.dispatch('loadCategories')
+    const records = await this.$store.dispatch('loadRecords')
+
+    this.categories = categories.map(cat => {
+      const spent = records
+        .filter(r => r.categoryId === cat.id)
+        .filter(r => r.type === 'outcome')
+        .reduce((total, record) => {
+          return total += +record.amount
+        }, 0)
+
+      const percent = 100 * spent / cat.limit
+      const progressPercent = percent > 100 ? 100 : percent
+      const progressColor = percent < 60
+          ? 'green'
+          : percent < 100
+              ? 'yellow'
+              : 'red'
+
+      return {
+        ...cat,
+        spent,
+        progressPercent,
+        progressColor
+      }
+    })
+
+    this.loading = false
+  }
 }
 </script>
 
